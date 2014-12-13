6388ce351b28f08a7d3732e9940bd1594ee3c415
t-kanda <t-kanda@phybbit.com>
2014-12-13 16:38:23 +0900

    stock, user infoを変更

--- QiitaWars/JANQiitaUserInfoService.m
+++ QiitaWars/JANQiitaUserInfoService.m
@@ -8,6 +8,7 @@
 
 #import "JANQiitaUserInfoService.h"
 #import <JSONKit-NoWarning/JSONKit.h>
+#import "JANQiitaUserInfo.h"
 
 @implementation JANQiitaUserInfoService
 
@@ -48,5 +49,37 @@
     qiitaUserInfo.itemsCount = [dict[@"items_count"] integerValue];
     return qiitaUserInfo;
 }
+- (void)saveQiitaUserInfo:(JANQiitaUserInfo*) qiitaUserInfo
+{
+    NSData* data = [NSKeyedArchiver archivedDataWithRootObject:qiitaUserInfo];
+    
+    NSMutableArray *qiitaUserInfos = [NSMutableArray arrayWithArray:[self loadQiitaUserInfos]];
+    [qiitaUserInfos addObject:data];
+    [self saveQiitaUserInfos:qiitaUserInfos];
+    
+}
 
+-(JANQiitaUserInfo *)lastQiitaUserInfo
+{
+    NSArray *qiitaUserInfos = [self loadQiitaUserInfos];
+    NSData* data = (NSData*)[qiitaUserInfos lastObject];
+    return [NSKeyedUnarchiver unarchiveObjectWithData:data];
+}
+
+-(NSArray *)loadQiitaUserInfos
+{
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSArray *qiitaUserInfos = [userDefaults objectForKey:QIITA_USER_INFO];
+    if (qiitaUserInfos) {
+        return [userDefaults objectForKey:QIITA_USER_INFO];
+    }
+    return [NSArray array];
+}
+
+- (void)saveQiitaUserInfos:(NSArray*)qiitaUserInfos
+{
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:qiitaUserInfos forKey:QIITA_USER_INFO];
+    [userDefaults synchronize];
+}
 @end
--- QiitaWars/Models/JANQiitaUserInfo.m
+++ QiitaWars/Models/JANQiitaUserInfo.m
@@ -9,5 +9,23 @@
 #import "JANQiitaUserInfo.h"
 
 @implementation JANQiitaUserInfo
-
+- (id)initWithCoder:(NSCoder *)coder {
+    if (self = [super init]) {
+        self.name = [coder decodeObjectForKey:@"name"];
+        self.qiitaId = [coder decodeObjectForKey:@"qiitaId"];
+        self.profileImageUrl = [coder decodeObjectForKey:@"profileImageUrl"];
+        self.followersCount = [coder decodeIntegerForKey:@"followersCount"];
+        self.followeesCount = [coder decodeIntegerForKey:@"followeesCount"];
+        self.itemsCount = [coder decodeIntegerForKey:@"itemsCount"];
+    }
+    return self;
+}
+- (void)encodeWithCoder:(NSCoder *)coder {
+    [coder encodeObject:_name forKey:@"name"];
+    [coder encodeObject:_qiitaId forKey:@"qiitaId"];
+    [coder encodeObject:_profileImageUrl forKey:@"profileImageUrl"];
+    [coder encodeInteger:_followersCount forKey:@"followersCount"];
+    [coder encodeInteger:_followeesCount forKey:@"followeesCount"];
+    [coder encodeInteger:_itemsCount forKey:@"itemsCount"];
+}
 @end
