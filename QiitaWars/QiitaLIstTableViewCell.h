12dc093dcbbd0a0a7cef4db648f9bf420a730719
t-kanda <t-kanda@phybbit.com>
2014-12-13 17:24:00 +0900

    cellを作成

---
+++ QiitaWars/QiitaLIstTableViewCell.h
@@ -0,0 +1,26 @@
+//
+//  QiitaLIstTableViewCell.h
+//  QiitaWars
+//
+//  Created by 神田 on 2014/12/13.
+//  Copyright (c) 2014年 bob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QiitaLIstTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *userImageView;
+@property (weak, nonatomic) IBOutlet UILabel *accountNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalValueLabel;
+@property (weak, nonatomic) IBOutlet UILabel *contributeCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *stockCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *folloeesCountLavel;
+@property (weak, nonatomic) IBOutlet UIImageView *contributeCountBackImageView;
+
+- (void)setStockCount:(NSInteger)stockCount;
+- (void)setFolloeesCount:(NSInteger)folloeesCount;
+- (void)setContributeCount:(NSInteger)contributeCount;
+- (void)setTotalValue:(NSInteger)totalValue;
+
+
+@end
