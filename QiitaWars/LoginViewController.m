c0fe539cae66643e14b108c3e32e7eb553bc90cc
t-kanda <t-kanda@phybbit.com>
2014-12-23 14:19:05 +0900

    紙芝居を設定

--- QiitaWars/LoginViewController.m
+++ QiitaWars/LoginViewController.m
@@ -17,15 +17,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+//    self.navigationController.navigationBarHidden = YES;
     // Do any additional setup after loading the view, typically from a nib.
     self.title = @"ログイン";
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    scrollView.contentSize = CGSizeMake(scrollView.frame.size.width * 3, scrollView.frame.size.height - 60);
+    
+    [scrollView setPagingEnabled:YES];
+
+    
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"splash"]];
+    [imageView setFrame:CGRectMake(imageView.frame.size.width*0, 0, imageView.frame.size.width, imageView.frame.size.height)];
+    [scrollView addSubview:imageView];
+    
+    imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"concept1"]];
+    [imageView setFrame:CGRectMake(imageView.frame.size.width*1, 0, imageView.frame.size.width, imageView.frame.size.height)];
+    [scrollView addSubview:imageView];
+    
+    imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"concept"]];
+    [imageView setFrame:CGRectMake(imageView.frame.size.width*2, 0, imageView.frame.size.width, imageView.frame.size.height)];
+    [scrollView addSubview:imageView];
+    
+    
+    
+    [self.view addSubview:scrollView];
+    
+    
+    
+    //右側にカメラボタンを表示する
+    UIBarButtonItem *btn = [[UIBarButtonItem alloc]
+                             initWithBarButtonSystemItem:UIBarButtonSystemItemPlay
+                             target:self action:@selector(hoge:)] ;
+    self.navigationItem.rightBarButtonItem = btn;
     
 }
+
+//カメラボタンが押されたときに呼ばれるメソッド
+-(void)hoge:(UIBarButtonItem*)b{
+    [self.navigationController pushViewController:[[QiitaListViewController alloc] init] animated:YES];
+}
+
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    [self.navigationController pushViewController:[[QiitaListViewController alloc] init] animated:YES];
+//    [self.navigationController pushViewController:[[QiitaListViewController alloc] init] animated:YES];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
