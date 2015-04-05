9a74403f80abc66af00e795e9b7f6ec6daaf4372
bob1983 <bob.1983.g@gmail.com>
2015-04-05 15:18:50 +0900

    OCMockを追加

---
+++ QiitaMeterTests/JanQiitaUserViewModelTestCase.m
@@ -0,0 +1,36 @@
+//
+//  JanQiitaUserViewModelTestCase.m
+//  QiitaMeter
+//
+//  Created by bob on 2015/04/05.
+//  Copyright (c) 2015年 bob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import "JANQiitaUserViewModel.h"
+#import "JANQiitaUserInfo.h"
+#import "JANPoint.h"
+
+@interface JanQiitaUserViewModelTestCase : XCTestCase
+
+@end
+
+@implementation JanQiitaUserViewModelTestCase
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testInitialize
+{
+    
+}
+@end
